9bba2fb980f8c1b09d05c538c9f111e262709677
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-15 21:42:06 +0700

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+docker rm -f tugas4-mysql:latest
 docker build -t tugas4-mysql ./mysql
+docker rm -f tugas4-app:latest
 docker build -t tugas4-app ./app
+docker rm -f tugas4-phpmyadmin:latest
 docker build -t tugas4-phpmyadmin ./phpmyadmin
+docker rm -f tugas4-alpine:latest
 docker build -t tugas4-alpine ./alpine
 docker network create example1-network
--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-docker run -d --name mysql1 --network example1-network \
+
+docker rm -f mysql1_tugas4 app_tugas4 phpmyadmin_tugas4 alpine_tugas4
+
+docker run -dit --name mysql1_tugas4 --network example1-network \
   -e MYSQL_DATABASE=mydb_tugas4 \
   -e MYSQL_PASSWORD=mydb6789tyui \
   -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-mysql
 
-docker run -d --name app --network example1-network \
+docker run -dit --name app_tugas4 --network example1-network \
   -p 4444:80 \
   -e MODE=development \
   -e MYSQL_DATABASE=mydb_tugas4 \
@@ -12,11 +15,11 @@ docker run -d --name app --network example1-network \
   -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-app
 
-docker run -d --name phpmyadmin --network example1-network \
+docker run -dit --name phpmyadmin_tugas4 --network example1-network \
   -p 44440:80 \
   -e PMA_HOST=mysql1 \
   -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-phpmyadmin
 
-docker run -d --name alpine --network example1-network \
+docker run -dit --name alpine_tugas4 --network example1-network \
   tugas4-alpine
