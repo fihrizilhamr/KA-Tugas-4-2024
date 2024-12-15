e4f60cbd133f22802eb27c4041b4140c3392ea80
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-15 21:52:44 +0700

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-docker rm -f tugas4-mysql:latest
+docker rmi tugas4-app tugas4-mysql tugas4-alpine tugas4-phpmyadmin
+
 docker build -t tugas4-mysql ./mysql
-docker rm -f tugas4-app:latest
 docker build -t tugas4-app ./app
-docker rm -f tugas4-phpmyadmin:latest
 docker build -t tugas4-phpmyadmin ./phpmyadmin
-docker rm -f tugas4-alpine:latest
 docker build -t tugas4-alpine ./alpine
 docker network create example1-network
