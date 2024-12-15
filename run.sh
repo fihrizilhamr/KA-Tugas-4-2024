e88e278c9a7b4e962c2d54cda805e8d4927922e5
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-12-15 22:00:20 +0700

    update

--- run.sh
+++ run.sh
@@ -2,23 +2,14 @@
 docker rm -f mysql1_tugas4 app_tugas4 phpmyadmin_tugas4 alpine_tugas4
 
 docker run -dit --name mysql1_tugas4 --network example1-network \
-  -e MYSQL_DATABASE=mydb_tugas4 \
-  -e MYSQL_PASSWORD=mydb6789tyui \
-  -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-mysql
 
 docker run -dit --name app_tugas4 --network example1-network \
   -p 4444:80 \
-  -e MODE=development \
-  -e MYSQL_DATABASE=mydb_tugas4 \
-  -e MYSQL_PASSWORD=mydb6789tyui \
-  -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-app
 
 docker run -dit --name phpmyadmin_tugas4 --network example1-network \
   -p 44440:80 \
-  -e PMA_HOST=mysql1 \
-  -e MYSQL_ROOT_PASSWORD=mydb6789tyui \
   tugas4-phpmyadmin
 
 docker run -dit --name alpine_tugas4 --network example1-network \
